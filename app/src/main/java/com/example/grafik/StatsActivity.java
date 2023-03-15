e31e6c7afc438e0cabad3eedee90b13eac606839
Marcin Brzozowski <brzozowski593@gmail.com>
2023-03-15 20:56:21 +0100

    dupa

--- app/src/main/java/com/example/grafik/StatsActivity.java
+++ app/src/main/java/com/example/grafik/StatsActivity.java
@@ -6,16 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class StatsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText amount = null;
+    EditText time = null;
+    TextView result = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
         ((FloatingActionButton) findViewById(R.id.menuButton)).setOnClickListener(this);
+        ((Button) findViewById(R.id.button)).setOnClickListener(this);
+
+        amount = findViewById(R.id.amount);
+        time = findViewById(R.id.time);
+        result = findViewById(R.id.result);
     }
 
     @Override
@@ -24,6 +35,19 @@ public class StatsActivity extends AppCompatActivity implements View.OnClickList
             case R.id.menuButton:
                 Intent myIntent = new Intent(this, MenuActivity.class);
                 startActivity(myIntent);
+                break;
+            case R.id.button:
+                int a = Integer.parseInt(amount.getEditableText().toString());
+                int b = Integer.parseInt(time.getEditableText().toString());
+
+                if (b == 0) {
+                    result.setText("0 rekinów na minutę");
+                    break;
+                }
+
+                int vel = a / b;
+                result.setText(String.format("%d rekinów na minutę", vel));
+
                 break;
         }
     }
--- app/src/main/java/com/example/grafik/TimetableActivity.java
+++ app/src/main/java/com/example/grafik/TimetableActivity.java
@@ -3,12 +3,17 @@ package com.example.grafik;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,14 +25,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class TimetableActivity extends AppCompatActivity implements View.OnClickListener, CalendarView.OnDateChangeListener {
     ListView days = null;
-    Set<String> dates = new HashSet<String>();
-    ArrayList<String> dates2 = new ArrayList<String>();
-    ArrayAdapter<String> adapter = null;
+    HashMap<String, String> events = new HashMap<String, String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +40,9 @@ public class TimetableActivity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_timetable);
         ((FloatingActionButton) findViewById(R.id.menuButton)).setOnClickListener(this);
         ((CalendarView) findViewById(R.id.calendarView)).setOnDateChangeListener(this);
+        days = findViewById(R.id.list_view);
 
-        days = findViewById(R.id.workoutDaysList);
-        adapter = new ArrayAdapter<String>(this, R.layout.list_custom_text, dates2);
+        CustomAdapter adapter = new CustomAdapter(events);
         days.setAdapter(adapter);
     }
 
@@ -55,14 +60,34 @@ public class TimetableActivity extends AppCompatActivity implements View.OnClick
     public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
         DateTimeFormatter sdf = DateTimeFormatter.ofPattern("YYYY-MM-dd");
         String date = sdf.format(LocalDate.of(year, month + 1, dayOfMonth));
-        if (dates.contains(date)) {
-            dates.remove(date);
-        } else {
-            dates.add(date);
-        }
 
-        ArrayAdapter<String> a = new ArrayAdapter<String>(this, R.layout.list_custom_text, R.id.list_content, Arrays.asList(dates.toArray(new String[dates.size()])));
-        days.setAdapter(a);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        final EditText edittext = new EditText(this);
+        alert.setMessage("Wpisz opis dnia");
+        alert.setTitle("Wydarzenie");
+        edittext.setHint("Opisz...");
+        edittext.setText(events.get(date));
+
+        alert.setView(edittext);
+
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String event_info = edittext.getText().toString();
+                events.put(date, event_info);
+
+                CustomAdapter adapter = new CustomAdapter(events);
+                days.setAdapter(adapter);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                return;
+            }
+        });
+
+
+        alert.show();
     }
 }
 
